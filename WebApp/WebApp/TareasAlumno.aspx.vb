06bf42ef4deba2112741bd35d7444a6ae5c1d66a
asierelursw <72493008+asierelursw@users.noreply.github.com>
2021-03-15 02:08:10 +0100

    Create TareasAlumno.aspx.vb

---
+++ WebApp/WebApp/TareasAlumno.aspx.vb
@@ -0,0 +1,29 @@
+﻿Imports DAO.AccesodatosSQL
+
+Public Class TareasAlumno
+    Inherits System.Web.UI.Page
+
+    Public Shared x As New DataTable
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        'Dim x = verTareas(GridView1.DataSourceID)
+        'DropDownList1.DataSource = VertareasAlumno(Session("Email"))
+
+        'Session.Add("TablaTareas", x.Table)
+        'GridView1.DataSource = x.Table
+        'GridView1.DataBind()
+        'Session.Add("TablaTareas", x)
+    End Sub
+
+    Protected Sub GridView1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub DropDownList1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DropDownList1.SelectedIndexChanged
+
+
+    End Sub
+
+    Protected Sub GridView1_SelectedIndexChanged1(sender As Object, e As EventArgs) Handles GridView1.SelectedIndexChanged
+
+    End Sub
+End Class
