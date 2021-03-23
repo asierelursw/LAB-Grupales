1c97f7b384d6157e7ce8afd7503490719d8d8cb5
asierelursw <72493008+asierelursw@users.noreply.github.com>
2021-03-23 23:00:52 +0100

    Update Profesor.aspx.designer.vb

--- WebApp/WebApp/Profesor.aspx.designer.vb
+++ WebApp/WebApp/Profesor.aspx.designer.vb
@@ -48,4 +48,22 @@ Partial Public Class Profesor
     '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
     '''</remarks>
     Protected WithEvents LinkButton2 As Global.System.Web.UI.WebControls.LinkButton
+
+    '''<summary>
+    '''Control LinkButton3.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents LinkButton3 As Global.System.Web.UI.WebControls.LinkButton
+
+    '''<summary>
+    '''Control LinkButton4.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents LinkButton4 As Global.System.Web.UI.WebControls.LinkButton
 End Class
